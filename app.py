d04c1461900df84264cf5e3507dde39b8551809e
Himanshu Kotkar <Himanshukotkar007@gmail.com>
2025-02-26 11:51:26 +0530

    added port

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ async def generate(request: Request, user_id):
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
